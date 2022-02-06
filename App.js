f43ce02c38d600ebced558afd1f417d500f5af87
Steve Rayner <srayner02@gmail.com>
2022-02-06 20:13:37 +0000

    Added listing details screen

--- App.js
+++ App.js
@@ -2,24 +2,9 @@ import React from "react";
 import { StyleSheet, View } from "react-native";
 import ViewImageScreen from "./app/screens/ViewImageScreen";
 import WelcomeScreen from "./app/screens/WelcomeScreen";
+import ListingDetailsScreen from "./app/screens/ListingDetailsScreen";
 import Card from "./app/components/Card";
 
 export default function App() {
-  return (
-    <View style={styles.container}>
-      <Card
-        title="Jacket for sale"
-        subTitle="£80"
-        image={require("./app/assets/jacket.jpg")}
-      ></Card>
-    </View>
-  );
+  return <ListingDetailsScreen />;
 }
-
-const styles = StyleSheet.create({
-  container: {
-    backgroundColor: "#f8f4f4",
-    padding: 20,
-    paddingTop: 100,
-  },
-});
