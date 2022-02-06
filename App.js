5b6a64dcf4548c0a6c52dc29c4d8d93493ee8cc4
Steve Rayner <srayner02@gmail.com>
2022-02-06 20:22:58 +0000

    added material icons

--- App.js
+++ App.js
@@ -6,5 +6,5 @@ import ListingDetailsScreen from "./app/screens/ListingDetailsScreen";
 import Card from "./app/components/Card";
 
 export default function App() {
-  return <ListingDetailsScreen />;
+  return <ViewImageScreen />;
 }
