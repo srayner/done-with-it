670c21789c00f3ef6bbb32b185c2b1c3ba165dda
Steve Rayner <srayner02@gmail.com>
2022-02-06 19:27:53 +0000

    Added Card component

--- App.js
+++ App.js
@@ -1,16 +1,25 @@
-import { StyleSheet, SafeAreaView } from "react-native";
+import React from "react";
+import { StyleSheet, View } from "react-native";
 import ViewImageScreen from "./app/screens/ViewImageScreen";
 import WelcomeScreen from "./app/screens/WelcomeScreen";
+import Card from "./app/components/Card";
 
 export default function App() {
-  return <WelcomeScreen />;
+  return (
+    <View style={styles.container}>
+      <Card
+        title="Jacket for sale"
+        subTitle="£80"
+        image={require("./app/assets/jacket.jpg")}
+      ></Card>
+    </View>
+  );
 }
 
 const styles = StyleSheet.create({
   container: {
-    backgroundColor: "white",
-    flex: 1,
-    justifyContent: "center",
-    alignItems: "center",
+    backgroundColor: "#f8f4f4",
+    padding: 20,
+    paddingTop: 100,
   },
 });
---
+++ app/components/AppText.js
@@ -0,0 +1,13 @@
+import { Platform, StyleSheet, Text } from "react-native";
+import React from "react";
+
+export default function AppText({ children, style }) {
+  return <Text style={[styles.text, style]}>{children}</Text>;
+}
+
+const styles = StyleSheet.create({
+  text: {
+    fontSize: 18,
+    fontFamily: Platform.OS === "android" ? "Roboto" : "Avenir",
+  },
+});
