fc7df23d6bbdd880c04060f72e595099b6870d0e
Steve Rayner <srayner02@gmail.com>
2022-02-06 18:07:04 +0000

    Complete welcome screen

--- App.js
+++ App.js
@@ -1,20 +1,16 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, SafeAreaView } from "react-native";
+import ViewImageScreen from "./app/screens/ViewImageScreen";
+import WelcomeScreen from "./app/screens/WelcomeScreen";
 
 export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+  return <WelcomeScreen />;
 }
 
 const styles = StyleSheet.create({
   container: {
+    backgroundColor: "white",
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    justifyContent: "center",
+    alignItems: "center",
   },
 });
---
+++ app/components/AppButton.js
@@ -0,0 +1,34 @@
+import React from "react";
+import { StyleSheet, TouchableOpacity, Text, View } from "react-native";
+
+import colors from "../config/colors";
+
+function AppButton({ title, onPress, color = "primary" }) {
+  return (
+    <TouchableOpacity
+      style={[styles.button, { backgroundColor: colors[color] }]}
+    >
+      <Text style={styles.text}>{title}</Text>
+    </TouchableOpacity>
+  );
+}
+
+const styles = StyleSheet.create({
+  button: {
+    borderRadius: 25,
+    backgroundColor: colors.primary,
+    padding: 15,
+    width: "100%",
+    justifyContent: "center",
+    alignItems: "center",
+    marginVertical: 10,
+  },
+  text: {
+    color: colors.white,
+    fontSize: 18,
+    fontWeight: "bold",
+    textTransform: "uppercase",
+  },
+});
+
+export default AppButton;
